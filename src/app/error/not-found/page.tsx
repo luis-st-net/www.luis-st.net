3cd297aaddc28de30b5f12c0f34d37829c05dd74
Luis <luis@staudt-fridingen.de>
2025-01-07 19:32:28 +0100

    removed custom error pages

--- src/app/error/not-found/page.tsx
+++
@@ -1,7 +0,0 @@
-import HttpStatusError from "@/lib/components/HttpStatusError";
-
-export default function () {
-	return (
-		<HttpStatusError message="Not Found" details="Ups, the page you are looking for does not exist." statusCode={404} width={400}/>
-	);
-}
