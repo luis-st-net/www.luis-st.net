4100d0bbeff376ef0e451833abc7e64ed32f9512
Luis <luis@staudt-fridingen.de>
2025-03-08 13:49:07 +0100

    added files for mods, projects, services and websites routes

---
+++ src/app/(website)/mods/xbackpack/page.tsx
@@ -0,0 +1,8 @@
+import styles from "./page.module.css";
+import React from "react";
+
+export default function () {
+	return (
+		<></>
+	);
+}
---
+++ src/app/(website)/services/mail-web-ui/layout.tsx
@@ -0,0 +1,10 @@
+import styles from "./layout.module.css";
+import React from "react";
+
+export default function (
+	{ children }: { children: React.ReactNode }
+) {
+	return (
+		<></>
+	);
+}
