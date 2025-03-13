c6d01993dc59db05ab4e18e7fd2cc8063b4f6ab9
Luis <luis@staudt-fridingen.de>
2025-03-13 20:58:02 +0100

    optimized navigation bar

--- src/lib/components/navigation-bar.tsx
+++ src/lib/components/navigation-bar.tsx
@@ -8,21 +8,20 @@ import { Route } from "@/lib/types";
 
 export default function NavigationBar() {
 	const path = usePathname().split("/").filter(s => s.length > 0);
-	const route = Routes.getRoute(path);
-	const parallelRoutes = Routes.getParallelRoutes(path);
-	const previousRoutes = Routes.getPreviousRoutes(path);
 	
 	return (
 		<div className="flex flex-row items-center h-14 m-4 mt-3 rounded-lg p-4 pr-2 bg-gradient-to-r from-custom-light-blue to-custom-green">
-			<Breadcrumb path={path} previousRoutes={previousRoutes} route={route} parallelRoutes={parallelRoutes}/>
+			<Breadcrumb path={path}/>
 			<Ui.ThemeToggle className="ml-auto"/>
 		</div>
 	);
 }
 
 function Breadcrumb(
-	{ path, previousRoutes, route, parallelRoutes }: { path: string[], previousRoutes: Route[], route: Route, parallelRoutes: Route[] },
+	{ path }: { path: string[] },
 ) {
+	const previousRoutes = Routes.getPreviousRoutes(path);
+	
 	return (
 		<Ui.Breadcrumb>
 			<Ui.BreadcrumbList>
@@ -31,7 +30,7 @@ function Breadcrumb(
 						<BreadcrumbItem key={index} path={path} route={route} index={index}/>
 					);
 				})}
-				<ActiveBreadcrumbItem path={path} route={route} parallelRoutes={parallelRoutes}/>
+				<ActiveBreadcrumbItem path={path}/>
 			</Ui.BreadcrumbList>
 		</Ui.Breadcrumb>
 	);
@@ -41,6 +40,7 @@ function BreadcrumbItem(
 	{ path, route, index }: { path: string[], route: Route, index: number },
 ) {
 	const url = "/" + path.slice(0, index).join("/");
+	
 	return (
 		<>
 			<Ui.BreadcrumbItem>
@@ -54,8 +54,11 @@ function BreadcrumbItem(
 }
 
 function ActiveBreadcrumbItem(
-	{ path, route, parallelRoutes }: { path: string[], route: Route, parallelRoutes: Route[] },
+	{ path }: { path: string[] },
 ) {
+	const route = Routes.getRoute(path);
+	const parallelRoutes = Routes.getParallelRoutes(path);
+	
 	if (parallelRoutes.length === 0) {
 		return (
 			<Ui.BreadcrumbItem>
@@ -90,6 +93,7 @@ function DropdownMenuItem(
 ) {
 	const currentPath = "/" + path.slice(0, path.length - 1).join("/");
 	const url = currentPath.length === 1 ? route.route : currentPath + route.route;
+	
 	return (
 		<Ui.DropdownMenuItem>
 			<Ui.BreadcrumbLink href={url} className="text-sm">
